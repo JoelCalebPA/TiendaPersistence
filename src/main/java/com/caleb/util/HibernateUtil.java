7416b15a78666f8f6dd7072b5e8ccaabb025c07f
Joel Caleb Paytan Arias <joel.caleb.pa@gmail.com>
2016-09-20 14:55:44 -0500

    HiberanteUtil

--- src/main/java/com/caleb/util/HibernateUtil.java
+++ src/main/java/com/caleb/util/HibernateUtil.java
@@ -1,9 +1,32 @@
 package com.caleb.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
 	private static SessionFactory sessionFactory;
+	
+	private static SessionFactory crearSessionFactory() {
+		try {
+			// Crea el sessionFactory desde el archivo de configuración de Hibernate
+			Configuration configuration = new Configuration();
+			configuration.configure("hibernate.cfg.xml");
+			
+			sessionFactory = configuration.buildSessionFactory();
+			
+			return sessionFactory;
+		} catch (Exception ex) {
+			System.err.println("La creación del sessionFactory a fallado." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+	
+	public SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			sessionFactory = crearSessionFactory();
+		}
+		return sessionFactory;
+	}
 	
 }
